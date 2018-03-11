35e194f7222731217d357c42de510233af9faee0
Arturo Rosete <arturorosete0@gmail.com>
2018-03-11 15:44:25 -0600

    Solucionado el no agregar alert level char. Se debe indicar la longitud mínima de la característica, al parecer.

--- hygiea_ble.ino
+++ hygiea_ble.ino
@@ -73,14 +73,14 @@ void setup()
   }
 
   /* Add User Battery level service definition */
-  Serial.println(F("Adding the Battery Level Service definition (UUID = 0x180F): "));
+  Serial.println(F("Adding the Battery Level Service definition(UUID=0x180F): "));
   success = ble.sendCommandWithIntReply(F("AT+GATTADDSERVICE=UUID=0x180F"), &battServiceId); 
   if (! success){
     error(F("Could not add User Data service"));
   }
 
     /* Add battery level characteristic */
-    Serial.println(F("Adding the Battery Level characteristic (UUID = 0x2A19): "));
+    Serial.println(F("Adding the Battery Level characteristic(UUID=0x2A19): "));
     success = ble.sendCommandWithIntReply(F("AT+GATTADDCHAR=UUID=0x2A19, PROPERTIES=0x10, MIN_LEN=1, VALUE=100"), &battCharId);
     if (! success) {
       error(F("Could not add Battery Level characteristic"));
@@ -88,46 +88,46 @@ void setup()
 
 
   /* Add the Immediate Alert Service definition */
-  Serial.println(F("Adding the Immediate Alert Service definition (UUID = 0x1802): "));
+  Serial.println(F("Adding the Immediate Alert Service definition(UUID=0x1802): "));
   success = ble.sendCommandWithIntReply( F("AT+GATTADDSERVICE=UUID=0x1802"), &iAServiceId);
   if (! success) {
     error(F("Could not add Immediate Alert service"));
   }
     /* Add the Alert Level characteristic */
-    Serial.println(F("Adding the Alert Level characteristic (UUID = 0x2A06): "));
-    success = ble.sendCommandWithIntReply( F("AT+GATTADDCHAR=UUID=0x2A06,VALUE=0"), &alertLCharId);
+    Serial.println(F("Adding the Alert Level characteristic(UUID=0x2A06): "));
+    success = ble.sendCommandWithIntReply( F("AT+GATTADDCHAR=UUID=0x2A06,PROPERTIES=0x04,MIN_LEN=1,VALUE=0"), &alertLCharId);
     if (! success) {
       error(F("Could not add Alert Level characteristic"));
     }
 
   /* Add the Enviromental Sensing Service definition */
-  Serial.println(F("Adding the Enviromental Sensing Service definition (UUID = 0x181A): "));
+  Serial.println(F("Adding the Enviromental Sensing Service definition(UUID=0x181A): "));
   success = ble.sendCommandWithIntReply( F("AT+GATTADDSERVICE=UUID=0x181A"), &envSensing_ServiceID);
   if (! success) {
     error(F("Could not add Enviromental Sensing service"));
   }
     /* Add the Elevation characteristic */
-    Serial.println(F("Adding the Elevation characteristic (UUID = 0x2A6C): "));
+    Serial.println(F("Adding the Elevation characteristic(UUID=0x2A6C): "));
     success = ble.sendCommandWithIntReply( F("AT+GATTADDCHAR=UUID=0x2A6C"), &elevation_CharID);
     if (! success) {
       error(F("Could not add Elevation characteristic"));
     }
 
     /* Add the Temperature characteristic */
-    Serial.println(F("Adding the Temperature characteristic (UUID = 0x2A6E): "));
+    Serial.println(F("Adding the Temperature characteristic(UUID=0x2A6E): "));
     success = ble.sendCommandWithIntReply( F("AT+GATTADDCHAR=UUID=0x2A6E"), &temp_CharID);
     if (! success) {
       error(F("Could not add Temperature characteristic"));
     }
 
   /* Add the Link Loss Service definition */
-  Serial.println(F("Adding the Link Loss Service definition (UUID = 0x1803): "));
+  Serial.println(F("Adding the Link Loss Service definition(UUID=0x1803): "));
   success = ble.sendCommandWithIntReply( F("AT+GATTADDSERVICE=UUID=0x1803"), &linkLoss_ServiceID);
   if (! success) {
     error(F("Could not add Link Loss service"));
   }
     /* Add the Link Loss Alert Level characteristic */
-    Serial.println(F("Adding the Link Loss Alert Level characteristic (UUID = 0x2A06): "));
+    Serial.println(F("Adding the Link Loss Alert Level characteristic(UUID=0x2A06): "));
     success = ble.sendCommandWithIntReply( F("AT+GATTADDCHAR=UUID=0x2A06,VALUE=0"), &ll_AlertLevel_CharID);
     if (! success) {
       error(F("Could not add Link Loss Alert Level characteristic"));
