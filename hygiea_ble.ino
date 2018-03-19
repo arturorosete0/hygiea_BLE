88bed0e3f881f1433a7f6298937f2fab71e2f8c6
Arturo Rosete <arturorosete0@gmail.com>
2018-03-19 16:32:47 -0600

    Test visual studio

--- hygiea_ble.ino
+++ hygiea_ble.ino
@@ -60,46 +60,46 @@ void repeatMe(){
     {
       Serial.println(F("Failed to get response!"));
     }*/
-}
+    }
 
-void setup()
-{
-  boolean success;
-  Serial.begin(115200);
-  
-  timer.setInterval(5000,repeatMe);
+    void setup()
+    {
+      boolean success;
+      Serial.begin(115200);
 
-  Wire.begin(5);
-  Wire.onReceive(receiveEvent);
-  randomSeed(micros());
+      timer.setInterval(5000,repeatMe);
+
+      Wire.begin(5);
+      Wire.onReceive(receiveEvent);
+      randomSeed(micros());
 
   /* Initialise the module */
-  if ( !ble.begin(VERBOSE_MODE) )
-  {
-    error(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?"));
-  }
+      if ( !ble.begin(VERBOSE_MODE) )
+      {
+        error(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?"));
+      }
 
   /* Perform a factory reset to make sure everything is in a known state */
-  if (! ble.factoryReset() ){
+      if (! ble.factoryReset() ){
        error(F("Couldn't factory reset"));
-  }
+     }
 
   /* Disable command echo from Bluefruit */
-  ble.echo(false);
+     ble.echo(false);
   /* Print Bluefruit information */
-  ble.info();
+     ble.info();
 
   /* Change the device name to make it easier to find */
-  if (! ble.sendCommandCheckOK(F("AT+GAPDEVNAME=Hygiea")) ) {
-    error(F("Could not set device name?"));
-  }
+     if (! ble.sendCommandCheckOK(F("AT+GAPDEVNAME=Hygiea")) ) {
+      error(F("Could not set device name?"));
+    }
 
   /* Add User Battery level service definition */
-  Serial.println(F("Adding the Battery Level Service definition(UUID=0x180F): "));
-  success = ble.sendCommandWithIntReply(F("AT+GATTADDSERVICE=UUID=0x180F"), &battServiceId); 
-  if (! success){
-    error(F("Could not add User Data service"));
-  }
+    Serial.println(F("Adding the Battery Level Service definition(UUID=0x180F): "));
+    success = ble.sendCommandWithIntReply(F("AT+GATTADDSERVICE=UUID=0x180F"), &battServiceId); 
+    if (! success){
+      error(F("Could not add User Data service"));
+    }
 
     /* Add battery level characteristic */
     Serial.println(F("Adding the Battery Level characteristic(UUID=0x2A19): "));
@@ -110,11 +110,11 @@ void setup()
 
 
   /* Add the Immediate Alert Service definition */
-  Serial.println(F("Adding the Immediate Alert Service definition(UUID=0x1802): "));
-  success = ble.sendCommandWithIntReply( F("AT+GATTADDSERVICE=UUID=0x1802"), &iAServiceId);
-  if (! success) {
-    error(F("Could not add Immediate Alert service"));
-  }
+    Serial.println(F("Adding the Immediate Alert Service definition(UUID=0x1802): "));
+    success = ble.sendCommandWithIntReply( F("AT+GATTADDSERVICE=UUID=0x1802"), &iAServiceId);
+    if (! success) {
+      error(F("Could not add Immediate Alert service"));
+    }
     /* Add the Alert Level characteristic */
     Serial.println(F("Adding the Alert Level characteristic(UUID=0x2A06): "));
     success = ble.sendCommandWithIntReply( F("AT+GATTADDCHAR=UUID=0x2A06,PROPERTIES=0x04,MIN_LEN=1,VALUE=0"), &alertLCharId);
@@ -123,11 +123,11 @@ void setup()
     }
 
   /* Add the Enviromental Sensing Service definition */
-  Serial.println(F("Adding the Enviromental Sensing Service definition(UUID=0x181A): "));
-  success = ble.sendCommandWithIntReply( F("AT+GATTADDSERVICE=UUID=0x181A"), &envSensing_ServiceID);
-  if (! success) {
-    error(F("Could not add Enviromental Sensing service"));
-  }
+    Serial.println(F("Adding the Enviromental Sensing Service definition(UUID=0x181A): "));
+    success = ble.sendCommandWithIntReply( F("AT+GATTADDSERVICE=UUID=0x181A"), &envSensing_ServiceID);
+    if (! success) {
+      error(F("Could not add Enviromental Sensing service"));
+    }
     /* Add the Elevation characteristic */
     Serial.println(F("Adding the Elevation characteristic(UUID=0x2A6C): "));
     success = ble.sendCommandWithIntReply( F("AT+GATTADDCHAR=UUID=0x2A6C,PROPERTIES=0x02,MIN_LEN=1"), &elevation_CharID);
@@ -143,11 +143,11 @@ void setup()
     }
 
   /* Add the Link Loss Service definition */
-  Serial.println(F("Adding the Link Loss Service definition(UUID=0x1803): "));
-  success = ble.sendCommandWithIntReply( F("AT+GATTADDSERVICE=UUID=0x1803"), &linkLoss_ServiceID);
-  if (! success) {
-    error(F("Could not add Link Loss service"));
-  }
+    Serial.println(F("Adding the Link Loss Service definition(UUID=0x1803): "));
+    success = ble.sendCommandWithIntReply( F("AT+GATTADDSERVICE=UUID=0x1803"), &linkLoss_ServiceID);
+    if (! success) {
+      error(F("Could not add Link Loss service"));
+    }
     /* Add the Link Loss Alert Level characteristic */
     Serial.println(F("Adding the Link Loss Alert Level characteristic(UUID=0x2A06): "));
     success = ble.sendCommandWithIntReply( F("AT+GATTADDCHAR=UUID=0x2A06,PROPERTIES=0x04,MIN_LEN=1,VALUE=0"), &ll_AlertLevel_CharID);
@@ -156,73 +156,78 @@ void setup()
     }
 
   /* Reset the device for new the service setting changes to take effect */
-  ble.reset();
-  Serial.println("Setup finished.");
-  delay(3000);
-}
-
-void loop()
-{
-  timer.run();  
-  if(current_version != last_version){
-    last_version = current_version;
-    update_ble(update_id, data_in[id]);    
+    ble.reset();
+    Serial.println("Setup finished.");
+    delay(3000);
   }
-}
 
-void receiveEvent(int howMany)
-{
-  int x = Wire.available();
-  int prev_data[] = {battery_level, alert_level, elevation, temperature};
-  for(int i=0; i<x; i++){
-      data_in[i] = Wire.read();    
-  }
-  id =diff_array(prev_data, data_in);
-  if(id != 6){
-    current_version = !current_version;
-    switch (id) {
-        case 0:
-          update_id = battCharId;
-          break;
-        case 1:
-          update_id = alertLCharId;
-          break;
-        case 2:
-          update_id = elevation_CharID;
-          break;
-        case 3:
-          update_id = temp_CharID;
-          break;
-        case 6:
-          Serial.println("Error: Diferente longitud.");
-          break;
+  void loop()
+  {
+    timer.run();  
+    if(current_version != last_version){
+      last_version = current_version;
+      update_ble(update_id, data_in[id]);
     }
   }
-}
 
-int diff_array(int *a, int *b){
-  int n;
-  //test length difference
-  if(sizeof(a)!=sizeof(b)){
-    Serial.println(F("Not same length."));
-    return 5;
-  }
-  //test each element to be the same. If not, return false
-  for(n=0; n<sizeof(a);n++){
-      if(a[n]!=b[n]){
-          return a[n];
+  void receiveEvent(int howMany)
+  {
+    Serial.println("Fired!");
+    int x = Wire.available();
+    int prev_data[] = {battery_level, alert_level, elevation, temperature};
+    for(int i=0; i<x; i++){
+      data_in[i] = Wire.read();    
+    }
+    id =diff_array(prev_data, data_in);
+    if(id != 6){
+      current_version = !current_version;
+      switch (id) {
+        case 0:
+        Serial.println("Bateria actualizada.");
+        update_id = battCharId;
+        break;
+        case 1:
+        Serial.println("Alerta actualizada.");
+        update_id = alertLCharId;
+        break;
+        case 2:
+        Serial.println("elevacion actualizada.");
+        update_id = elevation_CharID;
+        break;
+        case 3:
+        Serial.println("temperatura actualizada.");
+        update_id = temp_CharID;
+        break;
+        case 6:
+        Serial.println("Error: Diferente longitud.");
+        break;
       }
+    }
   }
-  return 6;
-}
 
-void update_ble(int char_id, int value){
-  ble.print( F("AT+GATTCHAR=") );
-  ble.print( char_id);
-  ble.print( F(",00-") );
-  ble.println(value, HEX);
-  if ( !ble.waitForOK() )
+  int diff_array(int *a, int *b){
+    int n;
+  //test length difference
+    if(sizeof(a)!=sizeof(b)){
+      Serial.println(F("Not same length."));
+      return 5;
+    }
+  //test each element to be the same. If not, return false
+    for(n=sizeof(b); n>0;n--){
+      if(a[n]!= b[n]){
+        return a[n];
+      }
+    }
+    return 6;
+  }
+
+  void update_ble(int char_id, int value){
+    ble.print( F("AT+GATTCHAR=") );
+    ble.print( char_id);
+    ble.print( F(",00-") );
+    ble.println(value, HEX);
+    if ( !ble.waitForOK() )
     {
       Serial.println(F("Failed to get response!"));
     }
-}
+  }
